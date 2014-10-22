3cbafdc9640ce246705e7732e3e3d5cf6f5a6fb6
saladman <saladmanspam@gmail.com>
2014-10-21 21:48:43 -0700

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -20,7 +20,8 @@
 
 - (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
     // this will get called every time the player touches the screen
+[character flap]
 }
-[Character flap];
+
 
 @end
